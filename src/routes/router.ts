0b71e68c34267c927b996c09cf0541fa33132fc8
Oscar-02 <70180715+Oscar-02@users.noreply.github.com>
2025-01-03 22:57:16 -0600

    feat: recurrent events crud
    * Added controller, service and route for recurring events
    
    * Added swagger. Fixed package.json
    
    * Added swagger
    
    * Fixed Swagger bug
    
    * Merge branch 'develop' of https://github.com/montanoo/backend-church into feature/CRUD-recurrentevent
    
    ---------
    
    Co-authored-by: Oscar-02 <oscacanas29gmail.com>

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from "express";
 import loginRouter from "./auth.routes";
 import parishRouter from "./parish.routes";
 import reservationNotificationRouter from "./rsv.noti.routes";
+import recurringEventRouter from "./recurringEvent.routes";
 import tokenRouter from "./token.routes";
 import parishEventRoutes from "./parishEvent.routes";
 
@@ -26,6 +27,8 @@ router.use("/auth", loginRouter);
 router.use("/parishes", parishRouter);
 
 router.use("/reservation-notifications", reservationNotificationRouter);
+
+router.use("/recurring-events", recurringEventRouter);
 
 router.use("/tokens", tokenRouter);
 router.use("/parish-events", parishEventRoutes);
